cfd1d64602da4458716bb457e9113c6322b5f205
Mommotexx <tex@duck.com>
2022-09-08 15:09:25 +0200

    Initial plugin setup

---
+++ src/main/java/com/marcusjohansen/myessentials/MyEssentials.java
@@ -0,0 +1,17 @@
+package com.marcusjohansen.myessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
