7f0e8b73e43c8c75857c73534231cd71bb7c4600
Mommotexx <tex@duck.com>
2022-09-08 19:32:27 +0200

    Added a basic permission check for /feed

--- src/main/java/com/marcusjohansen/myessentials/commands/FeedCommand.java
+++ src/main/java/com/marcusjohansen/myessentials/commands/FeedCommand.java
@@ -12,8 +12,13 @@ public class FeedCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player) {
             Player p = (Player) sender;
-            p.setFoodLevel(20);
-            p.sendMessage(ChatColor.GREEN + "Your hunger has been restored");
+            if (p.hasPermission("myessentials.feed")) {
+                p.setFoodLevel(20);
+                p.sendMessage(ChatColor.GREEN + "Your hunger has been restored");
+            } else {
+                p.sendMessage(ChatColor.RED + "You don't have the required permission to use this command!");
+            }
+
         }
         return true;
     }
